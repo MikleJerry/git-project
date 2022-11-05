64c677d1de1e1c4b6180aed044fdc67e125a90d1
MikleJerry <just_foramoment_539@mail.ru>
2022-11-05 13:43:29 +0300

    version 1

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ def main():
     print("Хочу кушать :)")
 
 def super_function():
-    print("ЭТО СУПЕ ФУУУУНКЦИЫ!!!")
+    print("ЭТО СУПЕ ФУУУУНКЦИЫ!!")
 
-def super_duper_function():
-    print("Хочу спать!")
 
+# Version 1
 
 if __name__ == '__main__':
     main()
